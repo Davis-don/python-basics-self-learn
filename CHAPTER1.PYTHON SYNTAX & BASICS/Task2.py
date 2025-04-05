710b0f6456486662eca571958e4046216d322d93
Davis-don <davismugoikou@gmail.com>
2025-04-05 12:03:39 +0300

    completed second task

---
+++ CHAPTER1.PYTHON SYNTAX & BASICS/Task2.py
@@ -0,0 +1,12 @@
+# Write script to perfom basic arithmetic operations (+,-,*,/) wit user provided numbers
+
+a=20
+b=30
+
+print(f"The two numbers added are {a+b}")
+
+print(f"The two numbers subtracted are {a-b}")
+
+print(f"The two numbers multiplied are {a*b}")
+
+print(f"The two numbers divided are {a/b}")
