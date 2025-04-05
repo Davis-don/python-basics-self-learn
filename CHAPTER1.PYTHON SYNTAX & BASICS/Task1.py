38258d7059dc4f93116d02c21edac559841bfc36
Davis-don <davismugoikou@gmail.com>
2025-04-05 11:58:09 +0300

    completed first task

---
+++ CHAPTER1.PYTHON SYNTAX & BASICS/Task1.py
@@ -0,0 +1,11 @@
+"""
+Create a program that accepts user input and print a personalized greetings
+"""
+#Ask user for a name
+user_name = input("please enter your name:\n")
+
+greetings = """Heloo """ + user_name + """
+Its nice to have you here
+"""
+
+print(greetings)
