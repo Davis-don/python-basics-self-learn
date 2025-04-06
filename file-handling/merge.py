698b63b1bd274392bdf0b10e9e71978f31ffd6c2
Davis-don <davismugoikou@gmail.com>
2025-04-06 17:02:18 +0300

    modified

---
+++ file-handling/merge.py
@@ -0,0 +1,5 @@
+
+# read data of demo
+f1 = open("demo.txt","r")
+demo1=f1.read()
+demo1=f1.read()
---
+++ file-handling/number_of_lines.py
@@ -0,0 +1,4 @@
+
+# Count number of lines in text file
+f = open("demo.txt","r")
+print(len(f.readlines()))
